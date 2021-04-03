b55543a0103777ef05c1cb6bd5918c5ab466c787
Vince Sangkhae <vsangk@gmail.com>
2021-04-02 23:40:39 -0700

    Add glob to design-system

--- design-system/components/index.js
+++ design-system/components/index.js
@@ -1,1 +1,11 @@
-export * from './button';
+import glob from 'glob';
+
+glob('../components/*.js', {}, function (er, files) {
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+  console.log(er, files);
+});
+
+export * from '../components';
